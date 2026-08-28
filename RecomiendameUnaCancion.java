0c4139ebb0e7c438fa826def0ce94a9b94d95bad
Leonardo <leoms5000@gmail.com>
2026-08-28 02:04:48 -0600

    Se agregó primera recomendacion musical

---
+++ RecomiendameUnaCancion.java
@@ -0,0 +1,16 @@
+/*
+ * Autor: Leonardo Manuel
+ * Asignatura: Modelos de Programación Orientada a Objetos
+ *
+ * Descripción:
+ * Programa colaborativo para compartir recomendaciones musicales.
+ */
+public class RecomiendameUnaCancion {
+    public static void main(String[] args) {
+        System.out.println("=== RECOMIÉNDAME UNA CANCIÓN ===\n");
+        System.out.println("Leonardo recomienda:");
+        System.out.println("Canción: Spiral");
+        System.out.println("Artista: LONGMAN");
+        System.out.println("¿Por qué?:La letra refleja el proceso de autodescubrimiento crecimiento personal a través de las experiencias y desafíos enfrentados.");
+    }
+}
