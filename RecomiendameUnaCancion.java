ec64245fe5f4d7beae3c93aa01cf0472c90c5345
Dialing1 <143584725+Dialing1@users.noreply.github.com>
2026-08-29 14:50:50 -0600

    Add song recommendation by Armando Mancilla
    Added a new song recommendation by Armando Mancilla.

--- RecomiendameUnaCancion.java
+++ RecomiendameUnaCancion.java
@@ -5,12 +5,21 @@
  * Descripción:
  * Programa colaborativo para compartir recomendaciones musicales.
  */
-public class RecomiendameUnaCancion {
+public class RecomiendameUnaCancionprueba {
     public static void main(String[] args) {
         System.out.println("=== RECOMIÉNDAME UNA CANCIÓN ===\n");
         System.out.println("Leonardo recomienda:");
         System.out.println("Canción: Spiral");
         System.out.println("Artista: LONGMAN");
         System.out.println("¿Por qué?:La letra refleja el proceso de autodescubrimiento crecimiento personal a través de las experiencias y desafíos enfrentados.");
+    
+    // Recomendación agregada por Armando Mancilla
+System.out.println();
+System.out.println("Armando Recomienda");
+System.out.println("Cancion: Roads Untraveled");
+System.out.println("Artista: Linkin Park");
+System.out.println("Por que? Porque para manejar es lo mejor");
+    
+    
     }
 }
